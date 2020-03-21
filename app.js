262f482091f17865d440063f97f2f5246a67bd73
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-03-21 12:19:06 -0400

    Add letters needed for my phone

--- app.js
+++ app.js
@@ -18,11 +18,12 @@ const stringToSVG = (str, pxSize=.5, width=100, lineHeight=12) => {
   // Maps for irregular letter sizes (regular is 4px wide by 5px tall)
   // Values are [width, height] in px
   const irregLtr = {
-    '@': [5,7],
-    'v': [3,5],
-    'i': [3,5],
     '.': [2,3],
+    '-': [3,1],
+    '@': [5,7],
+    'i': [3,5],
     'm': [5,5],
+    'v': [3,5],
   }
 
   // Use above to arrange and fill in the characters!
@@ -46,7 +47,7 @@ const stringToSVG = (str, pxSize=.5, width=100, lineHeight=12) => {
       row += lineHeight;
     }
     // Find y starting position -- offset is hard-coded to align letters & punctuation to baseline
-    const yOff = lh === 7 ? 0 : lh === 5 ? 1 : 4;
+    const yOff = lh === 7 ? 0 : lh === 5 ? 1 : lh === 1 ? 3 : 4;
     y = row + yOff;
 
     // Set the points for the svg
@@ -78,7 +79,14 @@ const stringToSVG = (str, pxSize=.5, width=100, lineHeight=12) => {
 const getLetterPoints = (l,x,y) => {
   const lookup = {
     '.': `${x},${y} ${x+2},${y} ${x+2},${y+2} ${x},${y+2}`,
+    '-': `${x},${y} ${x+3},${y} ${x+3},${y+1} ${x},${y+1}`,
     '@': `${x+4},${y+4} ${x+4},${y+3} ${x+3},${y+3} ${x+3},${y+4} ${x+5},${y+4} ${x+5},${y+5} ${x+2},${y+5} ${x+2},${y+2} ${x+4},${y+2} ${x+4},${y+4} ${x+5},${y+4} ${x+5},${y+1} ${x+4},${y+1} ${x+4},${y} ${x+1},${y} ${x+1},${y+7} ${x+5},${y+7} ${x+5},${y+6} ${x},${y+6} ${x},${y+1} ${x+4},${y+1}`,
+    '0': `${x},${y} ${x+4},${y} ${x+4},${y+1} ${x},${y+1} ${x},${y+5} ${x+1},${y+5} ${x+1},${y+1} ${x+4},${y+1} ${x+4},${y+5} ${x+1},${y+5} ${x+1},${y+4} ${x+3},${y+4} ${x+3},${y+1} ${x},${y+1}`,
+    '2': `${x},${y} ${x+4},${y} ${x+4},${y+3} ${x+1},${y+3} ${x+1},${y+4} ${x+4},${y+4} ${x+4},${y+5} ${x},${y+5} ${x},${y+2} ${x+3},${y+2} ${x+3},${y+1} ${x},${y+1}`,
+    '6': `${x+1},${y+3} ${x+1},${y+2} ${x+4},${y+2} ${x+4},${y+5} ${x},${y+5} ${x},${y} ${x+3},${y} ${x+3},${y+1} ${x+1},${y+1} ${x+1},${y+4} ${x+3},${y+4} ${x+3},${y+3}`,
+    '7': `${x},${y} ${x+4},${y} ${x+4},${y+2} ${x+2},${y+2} ${x+2},${y+5} ${x+1},${y+5} ${x+1},${y+3} ${x+3},${y+3} ${x+3},${y+1} ${x},${y+1}`,
+    '8': `${x},${y} ${x+4},${y} ${x+4},${y+1} ${x},${y+1} ${x},${y+5} ${x+4},${y+5} ${x+4},${y+4} ${x+1},${y+4} ${x+1},${y+1} ${x+3},${y+1} ${x+3},${y+2} ${x+1},${y+2} ${x+1},${y+3} ${x+3},${y+3} ${x+3},${y+5} ${x+4},${y+5} ${x+4},${y} ${x},${y} ${x},${y} ${x},${y+1} ${x+4},${y+1} ${x+4},${y}`,
+    '9': `${x+3},${y+2} ${x+3},${y+3} ${x},${y+3} ${x},${y} ${x+4},${y} ${x+4},${y+5} ${x+3},${y+5} ${x+3},${y+1} ${x+1},${y+1} ${x+1},${y+2}`,
     'a': `${x},${y+1} ${x},${y+5} ${x+1},${y+5} ${x+1},${y+3} ${x+3},${y+3} ${x+3},${y+5} ${x+4},${y+5} ${x+4},${y+1} ${x+3},${y+1} ${x+3},${y+2} ${x+1},${y+2} ${x+1},${y} ${x+3},${y} ${x+3},${y+1}`,
     'c': `${x+1},${y} ${x+4},${y} ${x+4},${y+1} ${x},${y+1} ${x},${y+4} ${x+4},${y+4} ${x+4},${y+5}  ${x+1},${y+5}`,
     'd': `${x},${y} ${x+3},${y} ${x+3},${y+4} ${x+4},${y+4} ${x+4},${y+1} ${x},${y+1} ${x},${y+5} ${x+3},${y+5} ${x+3},${y+4} ${x+1},${y+4} ${x+1},${y+1} ${x},${y+1}`,
@@ -96,4 +104,4 @@ const getLetterPoints = (l,x,y) => {
   return l in lookup ? lookup[l] : false;
 }
 
-document.querySelector('.target').appendChild(stringToSVG('lUCas@ogdendavis.com'))
+document.querySelector('.target').appendChild(stringToSVG('720-772-8296'));
